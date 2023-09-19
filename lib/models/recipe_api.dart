bbb80a22adacf4971cabe61987e80c41a8a42e4d
sgc93 <smachewgedefaw@gmail.com>
2023-09-19 13:57:52 -0900

    update uri parameters

--- lib/models/recipe_api.dart
+++ lib/models/recipe_api.dart
@@ -5,10 +5,11 @@ import 'package:yummy_flutter/models/recipe.dart';
 
 class RecipeApi {
   static Future<List<Recipe>> getRecipe() async {
-    var uri = Uri.https('yummly2.p.rapidapi.com', 'feeds/list', { "limit": '24', "start": '0'});
+    var uri = Uri.https('yummly2.p.rapidapi.com', 'feeds/list', { "limit": "18", "start": "0", "tag": "list.recipe.popular"});
     final fetchedJson = await get(uri, headers: {
-    'X-RapidAPI-Key': 'e0eb433222mshe94ff72b7e662b9p1c4f14jsn6b4e63be9c85',
+    'X-RapidAPI-Key': 'YOUR API KEK',
     'X-RapidAPI-Host': 'yummly2.p.rapidapi.com',
+    "useQueryString": "true",
   });
     Map recipeMap = jsonDecode(fetchedJson.body);
     List recipeList = [];
