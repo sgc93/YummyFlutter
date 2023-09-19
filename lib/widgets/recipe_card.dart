28d669a660ff616bd71a87137da432188cc17957
sgc93 <smachewgedefaw@gmail.com>
2023-09-19 04:48:31 -0900

    create recipe displayer card

---
+++ lib/widgets/recipe_card.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class RecipeCard extends StatelessWidget {
+  const RecipeCard({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
