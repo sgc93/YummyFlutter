6cc3a59b586a815292f6d628e49445889007a042
sgc93 <smachewgedefaw@gmail.com>
2023-09-19 07:48:45 -0900

    add cutomized appbar to homepage

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:yummy_flutter/widgets/app_bar.dart';
 
 class HomePage extends StatefulWidget {
   const HomePage({super.key});
@@ -8,8 +9,14 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
+  late final  double _deviceWidth = MediaQuery.of(context).size.width;
+  late final  double _deviceHeight = MediaQuery.of(context).size.height;
+
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      backgroundColor:const Color.fromRGBO(26, 26, 47, 1),
+      appBar: CustomAppBar(height: _deviceHeight * 0.10),
+    );
   }
 }
