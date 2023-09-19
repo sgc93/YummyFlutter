bc4762a4f774c10f8c46c4caee0c85995924ae8c
sgc93 <smachewgedefaw@gmail.com>
2023-09-19 13:13:35 -0900

    add recipes in Listview

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:yummy_flutter/models/recipe.dart';
+import 'package:yummy_flutter/models/recipe_api.dart';
 import 'package:yummy_flutter/widgets/app_bar.dart';
 import 'package:yummy_flutter/widgets/recipe_card.dart';
 
@@ -10,19 +12,39 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
-  late final  double _deviceWidth = MediaQuery.of(context).size.width;
   late final  double _deviceHeight = MediaQuery.of(context).size.height;
+  List<Recipe> _recipeList = [] ;
+  bool _isLoading = true;
+
+  @override
+  void initState() {
+    super.initState();
+    getRecipes();
+  }
+
+  Future<void> getRecipes() async {
+    _recipeList = await RecipeApi.getRecipe();
+    setState(() {
+      _isLoading = false;
+    });
+    print(_recipeList);
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor:const Color.fromRGBO(26, 26, 47, 1),
       appBar: CustomAppBar(height: _deviceHeight * 0.10),
-      body: const RecipeCard(
-        title: 'https://lh3.googleusercontent.com/ei5eF1LRFkkcekhjdR_8XgOqgdjpomf-rda_vvh7jIauCgLlEWORINSKMRR6I6iTcxxZL9riJwFqKMvK0ixS0xwnRHGMY4I5Zw=s360', 
-        rating: '4.3', 
-        cookTime: '43 min', 
-        thumbnailUrl: 'https://lh3.googleusercontent.com/ei5eF1LRFkkcekhjdR_8XgOqgdjpomf-rda_vvh7jIauCgLlEWORINSKMRR6I6iTcxxZL9riJwFqKMvK0ixS0xwnRHGMY4I5Zw=s360'
+      body: _isLoading ? const Center(child: CircularProgressIndicator()) : ListView.builder(
+        itemCount: _recipeList.length,
+        itemBuilder: (context, int index){
+          return RecipeCard(
+            title: _recipeList[index].name,
+            rating: _recipeList[index].rating.toString(),
+            cookTime: _recipeList[index].totalTime,
+            thumbnailUrl: _recipeList[index].images,
+          );
+        }
       ),
     );
   }
