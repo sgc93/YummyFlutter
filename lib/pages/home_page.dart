57f59ccd4db6c2e94dfa115356b4ef0e55e3719e
sgc93 <smachewgedefaw@gmail.com>
2023-09-19 09:03:23 -0900

    add customized card to homepage

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:yummy_flutter/widgets/app_bar.dart';
+import 'package:yummy_flutter/widgets/recipe_card.dart';
 
 class HomePage extends StatefulWidget {
   const HomePage({super.key});
@@ -17,6 +18,12 @@ class _HomePageState extends State<HomePage> {
     return Scaffold(
       backgroundColor:const Color.fromRGBO(26, 26, 47, 1),
       appBar: CustomAppBar(height: _deviceHeight * 0.10),
+      body: const RecipeCard(
+        title: 'https://lh3.googleusercontent.com/ei5eF1LRFkkcekhjdR_8XgOqgdjpomf-rda_vvh7jIauCgLlEWORINSKMRR6I6iTcxxZL9riJwFqKMvK0ixS0xwnRHGMY4I5Zw=s360', 
+        rating: '4.3', 
+        cookTime: '43 min', 
+        thumbnailUrl: 'https://lh3.googleusercontent.com/ei5eF1LRFkkcekhjdR_8XgOqgdjpomf-rda_vvh7jIauCgLlEWORINSKMRR6I6iTcxxZL9riJwFqKMvK0ixS0xwnRHGMY4I5Zw=s360'
+      ),
     );
   }
 }
