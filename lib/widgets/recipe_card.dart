9ec306073391ee6035c70dafcd3ef16c0d54d646
sgc93 <smachewgedefaw@gmail.com>
2023-09-19 09:03:51 -0900

    construct customized recipe card

--- lib/widgets/recipe_card.dart
+++ lib/widgets/recipe_card.dart
@@ -1,10 +1,112 @@
 import 'package:flutter/material.dart';
 
 class RecipeCard extends StatelessWidget {
-  const RecipeCard({super.key});
+  final String title;
+  final String rating;
+  final String cookTime;
+  final String thumbnailUrl;
+
+  const RecipeCard({
+    super.key,
+    required this.title,
+    required this.rating,
+    required this.cookTime,
+    required this.thumbnailUrl,
+  });
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Container(
+      margin: const EdgeInsets.symmetric(horizontal: 20, vertical: 10),
+      width: MediaQuery.of(context).size.width,
+      height: 180,
+      decoration: BoxDecoration(
+        borderRadius: BorderRadius.circular(15),
+        image: DecorationImage(
+          colorFilter: ColorFilter.mode(
+            Colors.white.withOpacity(0.35),
+            BlendMode.dstATop,
+          ),
+          image: NetworkImage(thumbnailUrl),
+          fit: BoxFit.cover,
+        ),
+      ),
+      child: Stack(
+        children: [
+          Align(
+            alignment: Alignment.center,
+            child: Text(
+              title,
+              style: const TextStyle(
+                color: Colors.white,
+                fontSize: 16,
+                fontWeight: FontWeight.w800,
+              ),
+              overflow: TextOverflow.ellipsis,
+              maxLines: 2,
+              textAlign: TextAlign.center,
+            ),
+          ),
+          Align(
+            alignment: Alignment.bottomCenter,
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                Container(
+                  margin: const EdgeInsets.all(10),
+                  padding: const EdgeInsets.all(5),
+                  decoration: BoxDecoration(
+                    color: Colors.black38,
+                    borderRadius: BorderRadius.circular(8),
+                  ),
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      const Icon(
+                        Icons.star,
+                        color: Colors.white,
+                        size: 15,
+                      ),
+                      const SizedBox(width: 7,),
+                      Text(
+                        rating,
+                        style: const TextStyle(
+                          color: Colors.white,
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+                Container(
+                  margin: const EdgeInsets.all(10),
+                  padding: const EdgeInsets.all(5),
+                  decoration: BoxDecoration(
+                    color: Colors.black38,
+                    borderRadius: BorderRadius.circular(8),
+                  ),
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      const Icon(
+                        Icons.schedule,
+                        color: Colors.white,
+                        size: 15,
+                      ),
+                      const SizedBox(width: 7),
+                      Text(
+                        cookTime,
+                        style: const TextStyle(
+                          color: Colors.white,
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+              ],
+            ),
+          )
+        ],
+      ),
+    );
   }
 }
