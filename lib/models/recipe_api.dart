b202a32cf018202be40da208c90920d5acaa0dcb
sgc93 <smachewgedefaw@gmail.com>
2023-09-19 13:12:47 -0900

    remove uneccessary comments

--- lib/models/recipe_api.dart
+++ lib/models/recipe_api.dart
@@ -4,19 +4,6 @@ import 'package:http/http.dart' show get;
 import 'package:yummy_flutter/models/recipe.dart';
 
 class RecipeApi {
-//   const options = {
-//   method: 'GET',
-//   url: 'https://yummly2.p.rapidapi.com/feeds/list',
-//   params: {
-//     limit: '24',
-//     start: '0'
-//   },
-//   headers: {
-//     'X-RapidAPI-Key': 'e0eb433222mshe94ff72b7e662b9p1c4f14jsn6b4e63be9c85',
-//     'X-RapidAPI-Host': 'yummly2.p.rapidapi.com'
-//   }
-// };
-
   static Future<List<Recipe>> getRecipe() async {
     var uri = Uri.https('yummly2.p.rapidapi.com', 'feeds/list', { "limit": '24', "start": '0'});
     final fetchedJson = await get(uri, headers: {
@@ -29,8 +16,6 @@ class RecipeApi {
     for(var recipe in recipeMap['feed']){
       recipeList.add(recipe['content']['details']);
     }
-
     return Recipe.extractFetchedJson(recipeList);
   }
-
 }
