392b76db1e4df6aa330b46b56e6e3a09301dd23a
sgc93 <smachewgedefaw@gmail.com>
2023-09-19 11:41:46 -0900

    construc recipe model class

---
+++ lib/models/recipe.dart
@@ -0,0 +1,14 @@
+class Recipe {
+  final String name;
+  final String images;
+  final double rating;
+  final String totalTime;
+  
+  Recipe({
+    required this.name,
+    required this.images,
+    required this.rating,
+    required this.totalTime,
+  });
+
+}
